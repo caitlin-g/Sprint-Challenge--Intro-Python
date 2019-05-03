1190ecf024c83d5a66e7ce4983b5c4ec47dbaffa
caitlin-g <33031338+caitlin-g@users.noreply.github.com>
2019-05-03 11:57:13 -0400

    comp.py All names uppercase

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -67,7 +67,7 @@ print(f)
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names uppercase:")
-g = []
+g = [(x.name.upper(), x.age + 10) for x in humans]
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
